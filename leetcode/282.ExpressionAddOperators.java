3613b08845bc0f747ef5c8e7fba5fa379ec84ae8
Chaoran <superchaoran@gmail.com>
2016-12-23 11:19:12 -0800

    ExpressionAdd

---
+++ leetcode/282.ExpressionAddOperators.java
@@ -0,0 +1,60 @@
+/*
+Given a string that contains only digits 0-9 and a target value, return all possibilities to add binary operators (not unary) +, -, or * between the digits so they evaluate to the target value.
+
+Examples: 
+"123", 6 -> ["1+2+3", "1*2*3"] 
+"232", 8 -> ["2*3+2", "2+3*2"]
+"105", 5 -> ["1*0+5","10-5"]
+"00", 0 -> ["0+0", "0-0", "0*0"]
+"3456237490", 9191 -> []
+*/
+/*
+This problem has a lot of edge cases to be considered:
+
+overflow: we use a long type once it is larger than Integer.MAX_VALUE or minimum, we get over it.
+0 sequence: because we can't have numbers with multiple digits started with zero, we have to deal with it too.
+a little trick is that we should save the value that is to be multiplied in the next recursion.
+*/
+
+/*
+I am really curios to know how did you arrive at the equation of recursive expression for multiplication in the below line ?
+helper(rst, path + "*" + cur, num, target, i + 1, eval - multed + multed * cur, multed * cur );
+
+I couldn't get this expression and as a result my solution became very lengthy :)
+
+about a year ago
+19
+C czonzhu 
+Reputation:  516
+for example, if you have a sequence of 12345 and you have proceeded to 1 + 2 + 3, now your eval is 6 right? If you want to add a * between 3 and 4, you would take 3 as the digit to be multiplied, so you want to take it out from the existing eval. You have 1 + 2 + 3 * 4 and the eval now is (1 + 2 + 3) - 3 + (3 * 4). Hope this could help : )
+*/
+
+public class Solution {
+    public List<String> addOperators(String num, int target) {
+        List<String> rst = new ArrayList<String>();
+        if(num == null || num.length() == 0) return rst;
+        helper(rst, "", num, target, 0, 0, 0);
+        return rst;
+    }
+    public void helper(List<String> rst, String path, String num, int target, int pos, long eval, long multed){
+        if(pos == num.length()){
+            if(target == eval)
+                rst.add(path);
+            return;
+        }
+        for(int i = pos; i < num.length(); i++){
+            if(i != pos && num.charAt(pos) == '0') break;
+            long cur = Long.parseLong(num.substring(pos, i + 1));
+            if(pos == 0){
+                helper(rst, path + cur, num, target, i + 1, cur, cur);
+            }
+            else{
+                helper(rst, path + "+" + cur, num, target, i + 1, eval + cur , cur);
+                
+                helper(rst, path + "-" + cur, num, target, i + 1, eval -cur, -cur);
+                
+                helper(rst, path + "*" + cur, num, target, i + 1, eval - multed + multed * cur, multed * cur );
+            }
+        }
+    }
+}
