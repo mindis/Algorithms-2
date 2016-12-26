c8c0320b13b8d71f529e9a4b0a714d531b56d346
Chaoran <superchaoran@gmail.com>
2016-12-26 14:02:22 -0800

    generate parenthesis with backtracking

---
+++ leetcode/22.GenerateParentheses.java
@@ -0,0 +1,44 @@
+/*
+22. Generate Parentheses QuestionEditorial Solution  My Submissions
+Total Accepted: 121360
+Total Submissions: 292228
+Difficulty: Medium
+Contributors: Admin
+Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+
+For example, given n = 3, a solution set is:
+
+[
+  "((()))",
+  "(()())",
+  "(())()",
+  "()(())",
+  "()()()"
+]
+*/
+
+/*
+Easy to understand Java backtracking solution*/
+
+/*
+The idea here is to only add '(' and ')' that we know will guarantee us a solution (instead of adding 1 too many close). Once we add a '(' we will then discard it and try a ')' which can only close a valid '('. Each of these steps are recursively called.
+*/
+
+ public List<String> generateParenthesis(int n) {
+        List<String> list = new ArrayList<String>();
+        backtrack(list, "", 0, 0, n);
+        return list;
+    }
+    
+    public void backtrack(List<String> list, String str, int open, int close, int max){
+        
+        if(str.length() == max*2){
+            list.add(str);
+            return;
+        }
+        
+        if(open < max)
+            backtrack(list, str+"(", open+1, close, max);
+        if(close < open)
+            backtrack(list, str+")", open, close+1, max);
+    }
